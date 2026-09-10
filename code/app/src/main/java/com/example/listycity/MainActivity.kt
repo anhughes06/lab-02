a32a6777f994594b14f7c92a47d4bdf4a3b4655a
Andrew Hughes <anhughes@ualberta.ca>
2026-09-10 14:03:26 -0600

    Initial Commit

---
+++ code/app/src/main/java/com/example/listycity/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.example.listycity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.listycity.ui.theme.ListyCityTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val cityRepository = CityRepository()
+        setContent {
+            ListyCityTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CityListScreen(
+                        cities = cityRepository.cities,
+                        onAddCity = {cityRepository.addCity(it)},
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ListyCityTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun CityRow() {
+
+    Text(
+        text=city,
+        fontSize = 28.sp,
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 18.dp, vertical = 14.dp)
+    )
+
+
+
+}
+@Composable
+fun CityListScreen(cities: List<String>, onAddCity: (String) -> Unit, modifier: Modifier = Modifier) {
+
+    var newCityName by remember {mutableStateOf("")}
+
+    LazyColumn(modifier = modifier.fillMaxSize()) {
+
+        items(cities) { city ->
+            CityRow(city = city)
+        }
+
+    }
+
+}
+
+class CityRepository {
+
+    private val _cities = mutableStateListOf(
+
+        "Edmonton", "Vancouver", "Moscow",
+        "Sydney", "Berlin", "Vienna",
+        "Tokyo", "Beijing", "Osaka",
+        "New Delhi"
+
+    )
+
+    val cities: List<String>
+        get() = _cities
+
+    fun addCity(city: String) {
+        _cities.add(city)
+    }
+
+}
